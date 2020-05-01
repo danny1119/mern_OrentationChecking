60af45c883e349fd98fd7308751d0dfd0abafc37
an-nguyen239 <anguyen108@student.gsu.edu>
2020-05-01 00:58:10 -0400

    Add react

---
+++ client/src/App.js
@@ -0,0 +1,121 @@
+import React ,{ useMemo, useState, useEffect } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import School from '@material-ui/icons/School';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+import FileUpload from './component/FileUpload';
+import Table from './component/Table';
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
+const cards = [1, 2, 3];
+
+const App = () => {
+  const classes = useStyles();
+  const [showResults, setShowResults] = React.useState(false)
+  const onClick = () => setShowResults(true)
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <School className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Georgia State University
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              GSU Orientation
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+             New Student Orientation is Georgia State’s official undergraduate student orientation program. Your virtual experience will be hosted on your selected orientation date
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <FileUpload />
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://orientation.gsu.edu/files/2019/02/orientation-home-bg-6.jpg"
+                    title="Gsu students"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      GSU Event
+                    </Typography>
+                    <Typography>
+                      This is the event of GSU. You can check-in student in here.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary" onClick={onClick}>
+                      View
+                    </Button>
+                    
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+          <br/>
+          <br/>
+          { showResults ? <Table /> : null }
+        </Container>
+      </main>
+    
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/component/Table.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+import MaterialTable from 'material-table';
+
+export default function Table() {
+  const [state, setState] = React.useState( {
+    columns: [
+      {
+        title: 'Name',
+        field: 'name'
+      },
+      {
+        title: 'Surname',
+        field: 'surname'
+      },
+      {
+        title: 'Birth Year',
+        field: 'birthYear',
+        type: 'numeric'
+      },
+      {
+        title: 'Attending',
+        field: 'attendance',
+      },
+    ],
+    data: [
+      {
+        name: 'Thien An',
+        surname: 'Nguyen',
+        birthYear: 1996,
+        attendance: 'no'
+      },
+      {
+        name: 'Binh',
+        surname: 'Trinh',
+        birthYear: 2017,
+        attendance: 'yes',
+      },
+      {
+        name: 'Chung',
+        surname: 'Mai',
+        birthYear: 2016,
+        attendance: 'yes',
+      },
+      {
+        name: 'Danh',
+        surname: 'Nguyen',
+        birthYear: 1111,
+        attendance: 'yes',
+      },
+      {
+        name: 'Yen',
+        surname: 'Vo',
+        birthYear: 2015,
+        attendance: 'yes',
+      },
+      {
+        name: 'Ngoc',
+        surname: 'Truong',
+        birthYear: 2017,
+        attendance: 'yes',
+      },
+    ],
+  } );
+
+  return (
+  <MaterialTable title="Student table"
+                 columns={ state.columns }
+                 data={ state.data }
+                 editable={ { 
+                 onRowUpdate: (newData, oldData) =>
+                 new Promise((resolve) => {
+                   setTimeout(() => {
+                     resolve();
+                     if (oldData) {
+                       setState((prevState) => {
+                         const data = [...prevState.data];
+                         data[data.indexOf(oldData)] = newData;
+                         return { ...prevState, data };
+                       });
+                     }
+                   }, 600);
+                 }),
+  }}
+/>
+  );
+}
